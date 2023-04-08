3d3c6459e3f6ba7ab800cd4892546a67134f8d8f
Mikey <Th3M1k3y@users.noreply.github.com>
2023-04-08 14:57:14 +0200

    Update m106_normalizer.py

--- m106_normalizer.py
+++ m106_normalizer.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     for line in lines:
         if "M106" in line:            
             S = re.search(r'S(\d{1,3})', line)
-            speed = f'S{round((float(s_from_min) - float(s_from_min)) * (float(s_to_max) - float(s_to_min)) / (float(s_from_max) - float(s_from_min)) + float(s_to_min),decimals)}'                    
+            speed = f'S{round((float(S[0][1:]) - float(s_from_min)) * (float(s_to_max) - float(s_to_min)) / (float(s_from_max) - float(s_from_min)) + float(s_to_min),decimals)}'
             line = re.sub(r'S\d+', speed, line, flags=re.MULTILINE)
         output_file.write(line)
     output_file.close()
